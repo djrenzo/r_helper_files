f0857f290a83c77c8c4b436a7a1d4fd5e0c8fc95
djrenzo <31690288+djrenzo@users.noreply.github.com>
2021-03-09 22:41:38 +0100

    Create push.R

---
+++ push.R
@@ -0,0 +1,7 @@
+push <- function(msg=toString(now())) {
+  library(lubridate)
+  setwd("~/timeseries")
+  system("git add --all")
+  system(paste0("git commit -m '", msg, "'"))
+  system("git push")
+}
