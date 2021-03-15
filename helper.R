baa29d4db820da49c0b98711c4d158cd9b6c98d9
djrenzo <31690288+djrenzo@users.noreply.github.com>
2021-03-15 11:22:53 +0100

    Update helper.R

--- helper.R
+++ helper.R
@@ -3,3 +3,8 @@ quiet <- function(x) {
   on.exit(sink()) 
   invisible(force(x)) 
 }
+
+# Computing the mean, standard deviation, kurtosis and skewness of the data.
+desc_stats <- function(data){
+  data.frame(mean=mean(data), sd.dev = sd(data), min= min(data), 
+  max = max(data), skewness = skewness(data), kurtosis = kurtosis(data))}
