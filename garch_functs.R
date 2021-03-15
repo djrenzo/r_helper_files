a90ee2327497e39890f56f6d48653bda0f4094a8
djrenzo <31690288+djrenzo@users.noreply.github.com>
2021-03-15 11:24:47 +0100

    Update garch_functs.R

--- garch_functs.R
+++ garch_functs.R
@@ -261,22 +261,3 @@ extract.rugarch <- function(fit,
   return(tr)
 }
 
-to_markdown <- function(m, cust_row, cust_names, dig){
-  m = matrixreg(m,
-          custom.gof.rows = cust_row,
-          custom.model.names = cust_names, 
-          digits = dig)
-  colnames(m) = m[1,]
-  rownames(m) = m[,1]
-  return(m[-1,-1])
-}
-
-to_markdown_k <- function(m, cust_row, cust_names, dig){
-  m = matrixreg(m,
-          custom.gof.rows = cust_row,
-          custom.model.names = cust_names, 
-          digits = dig)
-  colnames(m) = m[1,]
-  rownames(m) = m[,1]
-  return(knitr::kable(m[-1,-1]))
-}
